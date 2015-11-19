0721b0847a6c6f661173189fff357a44466dc3a8
computercam <cameron102012@gmail.com>
2015-11-19 17:19:00 +0000

    Adding Unity Files
    The 'Frist' Real commit

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	private Vector3 velocity = Vector3.zero;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//Get a movement Vec
+	public void Move (Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+	// Run every iteration
+	void FixedUpdate ()
+	{
+		PerformMovement();
+	}
+
+	//perform movement
+	void PerformMovement ()
+	{
+		if (velocity != Vector3.zero)
+		{
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+		}
+	}
+}
